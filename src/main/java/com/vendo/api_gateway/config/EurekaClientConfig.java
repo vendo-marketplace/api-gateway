e6f6a9598889ad48346205d15df89c4e6e600963
HaaaamBurger <gemasclashes@gmail.com>
2025-09-07 16:22:07 +0300

    api-gateway init

---
+++ src/main/java/com/vendo/api_gateway/config/EurekaClientConfig.java
@@ -0,0 +1,9 @@
+package com.vendo.api_gateway.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class EurekaClientConfig {
+}
---
+++ src/main/java/com/vendo/api_gateway/routes/UserRoutes.java
@@ -0,0 +1,20 @@
+package com.vendo.api_gateway.routes;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static com.vendo.common.Service.USER_SERVICE;
+
+@Configuration
+public class UserRoutes {
+
+    @Bean
+    public RouteLocator userRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(USER_SERVICE.getName(), r -> r.path("/auth/**")
+                        .uri("lb://%s".formatted(USER_SERVICE.getName())))
+                .build();
+    }
+}
