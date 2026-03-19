cdfee1c2a81734e65176afc031e1110c9942d11a
HaaaamBurger <gemasclashes@gmail.com>
2026-03-19 15:17:20 +0200

    dockerfile, ProductRoutes

--- src/main/java/com/vendo/api_gateway/routes/Routes.java
+++ src/main/java/com/vendo/api_gateway/routes/Routes.java
@@ -5,14 +5,18 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static com.vendo.core_lib.type.ServiceName.AUTH_SERVICE;
 import static com.vendo.core_lib.type.ServiceName.PRODUCT_SERVICE;
 
 @Configuration
-public class ProductRoutes {
+public class Routes {
 
     @Bean
     public RouteLocator authRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
+                .route(AUTH_SERVICE.getServiceName(), r -> r
+                        .path("/auth/**", "/password/**", "/verification/**")
+                        .uri("lb://%s".formatted(AUTH_SERVICE.getServiceName())))
                 .route(PRODUCT_SERVICE.getServiceName(), r -> r
                         .path("/categories/**", "/products/**")
                         .uri("lb://%s".formatted(PRODUCT_SERVICE.getServiceName())))
